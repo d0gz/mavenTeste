45d203ff5772296f51b13bf083ff1ab52af6afb5
Ricardo <ricardobatista893@gmail.com>
2023-10-18 12:40:39 -0300

    criando a classe de teste e o automatizado ali

---
+++ src/test/java/org/example/MainTest.java
@@ -0,0 +1,24 @@
+package org.example;
+
+import static org.junit.Assert.assertEquals;
+import java.util.HashSet;
+import java.util.Set;
+
+import org.junit.Test;
+
+public class MainTest
+{
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        Set<Integer> s1 = new HashSet<>();
+        s1.add(1);
+        s1.add(2);
+        s1.add(3);
+
+        String actual = s1.toString();
+        String expected = "[1, 2, 3]";
+
+        assertEquals( expected, actual );
+    }
+}
